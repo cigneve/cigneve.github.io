056a9acc648104e692035c722f8da49e6ef0e476
Yusuf Said AKTAN <chaos435435@gmail.com>
2022-10-02 04:56:19 +0000

    ttsci

---
+++ ttsci/precache-manifest.133c0d3a951f207a32c7a005e57f7358.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51bf0f22fbec0eac5bf24f166b9eeb68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2910d5a36515ecbe7a0b",
+    "url": "/static/css/main.77d228da.chunk.css"
+  },
+  {
+    "revision": "6f01a816d40e639cfcb6",
+    "url": "/static/js/2.1ab6a6d4.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.1ab6a6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2910d5a36515ecbe7a0b",
+    "url": "/static/js/main.0cb71a0a.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "1e9253e233dddd1de107c993b87b10b7",
+    "url": "/static/media/features-split-image-01.1e9253e2.png"
+  },
+  {
+    "revision": "1e9253e233dddd1de107c993b87b10b7",
+    "url": "/static/media/illustration-section-01.1e9253e2.png"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "2c6e47ef2db49e60c36af63516107f2c",
+    "url": "/static/media/ss1.2c6e47ef.png"
+  },
+  {
+    "revision": "72a00f27357ff7561f34688bd10c9ceb",
+    "url": "/static/media/ss2.72a00f27.png"
+  },
+  {
+    "revision": "3a2019b330adc51bf60059d43f0c0133",
+    "url": "/static/media/ss3.3a2019b3.png"
+  }
+]);
