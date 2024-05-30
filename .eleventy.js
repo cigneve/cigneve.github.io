5b4646ebef35b15d574ed3dc2162f5e0de08f206
cigneve <chaos435435@gmail.com>
2024-05-31 02:00:21 +0300

    add eleventy config

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+// .eleventy.js
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("assets");
+
+  return {
+    dir: {
+      input: "src",
+      output: "www",
+    }
+  };
+};
